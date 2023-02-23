b22fad37cd2fee2705eb044a904151763ec4d052
Hacker <hacker@Hackers-MacBook-Pro.local>
2023-02-23 05:05:46 -0800

    update complete getById endpoint

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import Koa from "koa";
-// import Router, {RouterContext} from "koa-compose";
+import Router, {RouterContext} from 'koa-router';
 import logger from "koa-logger";
 import json from "koa-json";
 
 import {router as articles} from "./routes/articles"
 
 const app: Koa = new Koa();
-// const router: Router = new Router();
+const router: Router = new Router();
 
-// const welcomeAPI = async (ctx: RouterContext, next:any) => {
-//     ctx.body = {message: "Welcome to the blog API!"};
-//     await next();
-// };
+const welcomeAPI = async (ctx: RouterContext, next:any) => {
+    ctx.body = {message: "Welcome to the blog API!"};
+    await next();
+};
 
-// router.get('/api/v1', welcomeAPI);
+router.get('/api/v1', welcomeAPI);
 
 app.use(logger());
 app.use(json());
-// app.use(router.routers());
+app.use(router.routes());
 app.use(articles.routes());
 
 app.listen(10888);
